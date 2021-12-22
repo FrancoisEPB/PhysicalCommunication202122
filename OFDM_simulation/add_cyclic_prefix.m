dd2fe9ccd525186157c7378c3ee0a5a34c8b0f75
FrancoisEPB <francois0406@hotmail.com>
2021-12-22 18:48:50 +0100

    Works on STO in OFDM

--- OFDM_simulation/add_cyclic_prefix.m
+++ OFDM_simulation/add_cyclic_prefix.m
@@ -5,8 +5,9 @@ function X_prefix = add_cyclic_prefix(X,sub_car)
 
 % Add cyclic prefix of length sub_car/8
 
-N = size(X);
+N = length(X);
 X_prefix = [];
+cyclic_pref_size = sub_car/8;
 for i=1:N/sub_car
     block = X(sub_car*(i-1)+1:sub_car*i);
     block_prefix = [block(end-cyclic_pref_size+1:end) block];
--- OFDM_simulation/remove_cyclic_prefix.m
+++ OFDM_simulation/remove_cyclic_prefix.m
@@ -8,12 +8,12 @@ function Xr = remove_cyclic_prefix(Xr_prefix,sub_car,N)
 
 Xr = [];
 index = 1;
-
+cyclic_pref_size = sub_car/8;
 
 for i=1:N/sub_car
     block = Xr_prefix(index:index+sub_car+cyclic_pref_size-1);
-    index = index + sub_car + 1;
+    index = index + sub_car + cyclic_pref_size;
     block_no_prefix = block(cyclic_pref_size+1:end);
-    X_r = [X_r block_no_prefix];
+    Xr = [Xr block_no_prefix];
     
 end
